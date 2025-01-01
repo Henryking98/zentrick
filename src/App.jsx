223b565ee69d9835a2dd20e36473e8eb421d36d4
Henry <94872110+Henryking98@users.noreply.github.com>
2024-12-31 20:54:23 -1100

    added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero.jsx"
 import About from "./components/About.jsx"
 import Features from "./components/Features.jsx";
 import Story from "./components/Story.jsx";
+import Contact from "./components/Contact.jsx";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
             <About />
             <Features />
             <Story />
+            <Contact />
         </main>
     )
 }
